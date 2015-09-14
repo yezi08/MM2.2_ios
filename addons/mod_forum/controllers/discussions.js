68f604185469eb1a6868f88850c94b077ed831cb
yezi08 <yezi.yang08@163.com>
2015-09-14 20:26:28 +0800

    MM2.2 iOS
    1st try

---
+++ addons/mod_forum/controllers/discussions.js
@@ -0,0 +1,117 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+angular.module('mm.addons.mod_forum')
+
+/**
+ * Forum discussion list controller.
+ *
+ * @module mm.addons.mod_forum
+ * @ngdoc controller
+ * @name mmaModForumDiscussionsCtrl
+ */
+.controller('mmaModForumDiscussionsCtrl', function($q, $scope, $stateParams, $mmaModForum, $mmCourse, $mmUtil,
+            mmUserProfileState) {
+    var module = $stateParams.module || {},
+        courseid = $stateParams.courseid,
+        forum,
+        page = 0;
+
+    $scope.title = module.name;
+    $scope.description = module.description;
+    $scope.moduleurl = module.url;
+    $scope.courseid = courseid;
+    $scope.userStateName = mmUserProfileState;
+
+    // Convenience function to get forum data and discussions.
+    function fetchForumDataAndDiscussions(refresh) {
+        return $mmaModForum.getForum(courseid, module.id).then(function(forumdata) {
+            if (forumdata) {
+                forum = forumdata;
+
+                $scope.title = forum.name || $scope.title;
+                $scope.description = forum.intro || $scope.description;
+                $scope.forum = forum;
+
+                return fetchDiscussions(refresh);
+            } else {
+                $mmUtil.showErrorModal('mma.mod_forum.errorgetforum', true);
+                return $q.reject();
+            }
+        }, function(message) {
+            $mmUtil.showErrorModal(message);
+            $scope.canLoadMore = false; // Set to false to prevent infinite calls with infinite-loading.
+            return $q.reject();
+        });
+    }
+
+    // Convenience function to get forum discussions.
+    function fetchDiscussions(refresh) {
+        if (refresh) {
+            page = 0;
+        }
+
+        return $mmaModForum.getDiscussions(forum.id, page).then(function(response) {
+            if (page == 0) {
+                $scope.discussions = response.discussions;
+            } else {
+                $scope.discussions = $scope.discussions.concat(response.discussions);
+            }
+
+            $scope.count = $scope.discussions.length;
+            $scope.canLoadMore = response.canLoadMore;
+            page++;
+
+            preFetchDiscussionsPosts(response.discussions);
+
+        }, function(message) {
+            $mmUtil.showErrorModal(message);
+            $scope.canLoadMore = false; // Set to false to prevent infinite calls with infinite-loading.
+            return $q.reject();
+        });
+    }
+
+    // Convenience function to prefetch the posts of each discussion, so they're available in offline mode.
+    function preFetchDiscussionsPosts(discussions) {
+        angular.forEach(discussions, function(discussion) {
+            var discussionid = discussion.discussion;
+            $mmaModForum.getDiscussionPosts(discussionid);
+        });
+    }
+
+    fetchForumDataAndDiscussions().then(function() {
+        $mmaModForum.logView(forum.id).then(function() {
+            $mmCourse.checkModuleCompletion(courseid, module.completionstatus);
+        });
+    }).finally(function() {
+        $scope.discussionsLoaded = true;
+    });
+
+    // Load more discussions.
+    $scope.loadMoreDiscussions = function() {
+        fetchDiscussions().finally(function() {
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    };
+
+    // Pull to refresh.
+    $scope.refreshDiscussions = function() {
+        var promise = forum ? $mmaModForum.invalidateDiscussionsList(courseid, forum.id) : $q.when();
+        promise.finally(function() {
+            fetchForumDataAndDiscussions(true).finally(function() {
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        });
+    };
+});
---
+++ errorreport.js
@@ -0,0 +1,127 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// We use this code for handling unexpected errors.
+// Using JS confirm function we are sure that the user get notified in a Mobile device.
+// This script should be added at the begining of the index.html and it should only use native javascript functions.
+
+var errors = [],
+    ignoredErrors = [
+        'Can\'t find variable: cordova',
+        'Uncaught DataError: Failed to execute \'put\' on \'IDBObjectStore\''
+    ],
+    ignoredFiles = [
+        'ydn.db/jsc/ydn.db-dev.js',
+        'www/cordova.js',
+        'lib/ionic/js/ionic.bundle.min.js',
+        'lib/ionic/js/ionic.bundle.js'
+    ];
+
+/**
+ * Check if an error should be ignored.
+ *
+ * @param  {String} message Error message.
+ * @param  {String} file    File where the error occurred.
+ * @return {Boolean}        True if it should be ignored, false otherwise.
+ */
+function shouldIgnore(message, file) {
+    for (var i = 0; i < ignoredErrors.length; i++) {
+        if (message.indexOf(ignoredErrors[i]) != -1) {
+            return true;
+        }
+    }
+    var filepath = file.replace(/\\/g, '/');
+    for (i = 0; i < ignoredFiles.length; i++) {
+        if (filepath.indexOf(ignoredFiles[i]) != -1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+window.onerror = function(msg, url, lineNumber) {
+    var errorReported = false,
+        reportedOnDBReady = false;
+
+    function getStorageAndReport(reportUrl, db) {
+        if (!reportedOnDBReady) {
+            reportedOnDBReady = true;
+            reportUrl = reportUrl + '&storage=' + encodeURIComponent(db.getType());
+            window.open(reportUrl, '_system');
+        }
+    }
+
+    function reportError() {
+        if (!errorReported) {
+            errorReported = true;
+            var reportUrl = 'http://prototype.moodle.net/mobile/feedback/mmfeedback.php?message=' + encodeURIComponent(msg) +
+                            '&file=' + encodeURIComponent(url) + '&line=' + lineNumber;
+
+            if (window.device) {
+                reportUrl = reportUrl + '&platform=' + window.device.platform + '&model=' + encodeURIComponent(window.device.model) +
+                                      '&osversion=' + window.device.version + '&cordova=' + window.device.cordova;
+            }
+            if (window.location) {
+                reportUrl = reportUrl + '&localurl=' + encodeURIComponent(window.location.href);
+            }
+            if (navigator.userAgent) {
+                reportUrl = reportUrl + '&useragent=' + encodeURIComponent(navigator.userAgent);
+            }
+            if (ydn.db.Storage) {
+                // Detect Storage type by default.
+                var db = new ydn.db.Storage('test', {}, {});
+                if (db && db.getType && db.onReady) {
+                    db.onReady(function() {
+                        getStorageAndReport(reportUrl, db);
+                    });
+                    setTimeout(function() {
+                        getStorageAndReport(reportUrl, db);
+                    }, 1000);
+                    return;
+                }
+            }
+
+            window.open(reportUrl, '_system');
+        }
+    }
+
+    if (!shouldIgnore(msg, url) && errors.indexOf(msg) == -1) {
+        errors.push(msg);
+        // Use setTimeout to prevent the following error if the app crashes right at the start:
+        // "The connection to the server was unsuccessful. (file:///android_asset/www/index.html)"
+        setTimeout(function() {
+            if (typeof msg == "string") {
+                var sendError,
+                    confirmmsg = 'Unexpected error, please accept to report the bug so we can work on fixing it ' +
+                                    '(Internet connection required).';
+
+                sendError = confirm(confirmmsg);
+                if (sendError) {
+                    // Wait for device ready so we can retrieve device data. In most cases device will already be ready.
+                    document.addEventListener('deviceready', reportError);
+                    // Report error if device ready isn't fired after 5 seconds.
+                    setTimeout(reportError, 5000);
+                }
+            }
+
+            // This may help debugging if we use logging apps in iOs or Android.
+            if (typeof console != "undefined" && typeof console.log == "function") {
+                console.log(msg);
+            }
+        }, 100);
+    }
+
+    // Let default error handler run.
+    return false;
+};
